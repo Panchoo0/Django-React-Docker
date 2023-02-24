45c61dd364eaaaaf917813a5d5c6f13981cdb5ea
Francisco Galdames <fgaldames98@gmail.com>
2023-02-24 00:15:17 -0300

    Añadido Postgres como base de datos como container y es ocupada por Django

--- Back/app/Users/views.py
+++ Back/app/Users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.contrib.auth.models import User
+
+from django.http import HttpResponse
+
+
+def index(request):
+    user = User.objects.filter(username="panchho")[0]
+    print(user)
+    # user = User.objects.create(username="panchho",pk=1)
+    return HttpResponse(user)
